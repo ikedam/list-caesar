79aeee8602154c76f50af63c4a4d2c7ebc630bf0
ikedam <devld@ikedam.jp>
2022-02-05 17:31:11 +0900

    Implement list-caesar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 
+interface Caesar {
+  shift: number;
+  word: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'list-caesar';
+  word = '';
+
+  caesar(base: string, shift: number): string {
+    for(let idx = 0; idx < base.length; ++idx) {
+      const c = base.charCodeAt(idx);
+      // A: 0x41
+      // Z: 0x5a
+      // a: 0x61
+      // z: 0x7a
+      if (0x41 <= c && c <= 0x5a) {
+        const codeDiff = c - 0x41;
+        const newCodeDiff = (codeDiff + shift) % 26;
+        const newLetter = String.fromCharCode(0x41 + newCodeDiff);
+        base = base.substring(0, idx) + newLetter + base.substring(idx + 1);
+      } else if(0x61 <= c && c <= 0x7a) {
+        const codeDiff = c - 0x61;
+        const newCodeDiff = (codeDiff + shift) % 26;
+        const newLetter = String.fromCharCode(0x61 + newCodeDiff);
+        base = base.substring(0, idx) + newLetter + base.substring(idx + 1);
+      }
+    }
+    return base;
+  }
+
+  get caesarList(): Caesar[] {
+    if (this.word === '') {
+      return [];
+    }
+    const ret: Caesar[] = [];
+    for (let shift = 0; shift < 26; ++shift) {
+      ret.push({
+        shift: shift,
+        word: this.caesar(this.word, shift),
+      });
+    }
+    return ret;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field'; 
+import { MatInputModule } from '@angular/material/input'; 
+import { MatTableModule } from '@angular/material/table'; 
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
